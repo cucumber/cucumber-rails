682fa9dcf5331e00539995cdb95979b8ccecc092
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2010-01-18 01:15:07 +0100

    Remove reference to removed element_locator. Add more capybara tags.

--- generators/cucumber/templates/support/webrat.rb
+++ generators/cucumber/templates/support/webrat.rb
@@ -1,6 +1,5 @@
 require 'webrat'
 require 'webrat/core/matchers'
-require 'cucumber/webrat/element_locator' # Deprecated in favor of #tableish - remove this line if you don't use #element_at or #table_at
 
 Webrat.configure do |config|
   config.mode = :rails
