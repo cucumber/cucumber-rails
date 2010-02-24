2a236f89c259a224b04231fe5d00c73af8f2fab0
Louis Rose <louismrose@gmail.com>
2010-02-24 17:01:59 +0100

    Support for both rspec-1.x and rspec-2.x

--- lib/cucumber/rails/rspec.rb
+++ lib/cucumber/rails/rspec.rb
@@ -1,10 +1,23 @@
 require 'cucumber/rails/world'
-require 'spec/expectations'
-require 'spec/rails'
 
-[Cucumber::Rails::World, ActionController::Integration::Session].each do |klass|
-  klass.class_eval do
-    include Spec::Matchers
-    include Spec::Rails::Matchers
+begin
+  require 'rspec/expectations'
+  require 'rspec/rails'
+
+  [Cucumber::Rails::World, ActionController::Integration::Session].each do |klass|
+    klass.class_eval do
+      include Rspec::Matchers
+      include Rspec::Rails::Matchers
+    end
+  end
+rescue LoadError => try_rspec_1
+  require 'spec/expectations'
+  require 'spec/rails'
+
+  [Cucumber::Rails::World, ActionController::Integration::Session].each do |klass|
+    klass.class_eval do
+      include Spec::Matchers
+      include Spec::Rails::Matchers
+    end
   end
 end
