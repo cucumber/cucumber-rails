17a5c3b981b925672c2dd264b11fdb8d7f551ffc
Mike Sassak <msassak@gmail.com>
2010-04-24 18:29:50 -0400

    Feature to test rerun profile

--- features/step_definitions/cucumber_rails_steps.rb
+++ features/step_definitions/cucumber_rails_steps.rb
@@ -5,3 +5,25 @@ Given /^I symlink this repo to "([^\"]*)"$/ do |target|
     FileUtils.ln_s(source, target)
   end
 end
+
+Given /^I have created a new Rails 2 app "([^\"]*)" with cucumber\-rails support$/ do |app_name|
+  steps %Q{
+    Given I am using rvm "1.8.7"
+    And I am using rvm gemset "cucumber-rails-2.3.5" with Gemfile:
+      """
+      gem 'rails', '2.3.5'
+      gem 'sqlite3-ruby', '1.2.5'
+      gem 'capybara', '0.3.6'
+      gem 'webrat'
+      gem 'rspec-rails'
+      gem 'cucumber'
+      gem 'database_cleaner'
+      gem 'culerity'
+      gem 'celerity'
+      """
+    And I successfully run "rails #{app_name}"
+    And I cd to "#{app_name}"
+    And I symlink this repo to "vendor/plugins/cucumber-rails"
+    And I successfully run "ruby script/generate cucumber --capybara"
+  }
+end
