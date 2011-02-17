397911dc3e24e1395aa6fb92e73b521b692384fc
James F. Herdman <james@celect.org>
2011-02-17 16:52:41 -0500

    Works with Capybara ~> 0.4
    Hat tip to geoffd123. I only spruced up his fix.

--- lib/cucumber/rails/capybara/select_dates_and_times.rb
+++ lib/cucumber/rails/capybara/select_dates_and_times.rb
@@ -4,16 +4,16 @@ module Cucumber
       module SelectDatesAndTimes
         def select_date(field, options = {})
           date = Date.parse(options[:with])
-          within(:xpath, ::Capybara::XPath.fieldset(field).append(%Q{//p[label[contains(., "#{field}")]]})) do
-            find(:xpath, '//select[contains(@id, "_1i")]').select(date.year)
+          within(:xpath, ::XPath::HTML.fieldset(field).append(%Q{//p[label[contains(., "#{field}")]]})) do
+            find(:xpath, '//select[contains(@id, "_1i")]').select(date.year.to_s)
             find(:xpath, '//select[contains(@id, "_2i")]').select(date.strftime('%B'))
-            find(:xpath, '//select[contains(@id, "_3i")]').select(date.day)
+            find(:xpath, '//select[contains(@id, "_3i")]').select(date.day.to_s)
           end
         end
       
         def select_time(field, options = {})
           time = Time.parse(options[:with])
-          within(:xpath, ::Capybara::XPath.fieldset(field).append(%Q{//p[label[contains(., "#{field}")]]})) do
+          within(:xpath, ::XPath::HTML.fieldset(field).append(%Q{//p[label[contains(., "#{field}")]]})) do
             find(:xpath, '//select[contains(@id, "_4i")]').select(time.hour.to_s.rjust(2,'0'))
             find(:xpath, '//select[contains(@id, "_5i")]').select(time.min.to_s.rjust(2,'0'))
           end
