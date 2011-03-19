60111af86565ad1ed64c7354d694046b9bcdab90
aslakhellesoy <aslak.hellesoy@gmail.com>
2011-03-19 18:52:56 +0000

    Fix select_date. Closes #93, Closes #99, Closes #100, Closes #101, Closes #109

--- features/select_dates.feature
+++ features/select_dates.feature
@@ -11,7 +11,7 @@ Feature: Allow Cucumber to rescue exceptions
         Scenario: Constitution on May 17
           Given I am on the new appointment page
           And I fill in "Norway's constitution" for "Name"
-          And I select "2009-02-20 15:10:00 UTC" as the "appointment" date and time
+          And I select "2009-02-20 15:10:00 UTC" as the "When" date and time
           And I press "Create Appointment"
           Then I should see "Norway's constitution"
           And I should see "2009-02-20 15:10:00 UTC"
@@ -20,6 +20,6 @@ Feature: Allow Cucumber to rescue exceptions
     Then it should pass with:
       """
       1 scenario (1 passed)
-      1 step (1 passed)
+      6 steps (6 passed)
       """
     
--- lib/cucumber/rails/capybara/select_dates_and_times.rb
+++ lib/cucumber/rails/capybara/select_dates_and_times.rb
@@ -6,17 +6,17 @@ module Cucumber
           date        = Date.parse(options[:with])
           base_dom_id = get_base_dom_id_from_label_tag(field)
 
-          find(:xpath, "//select[@id='#{base_dom_id}1i']").select(date.year.to_s)
-          find(:xpath, "//select[@id='#{base_dom_id}2i']").select(date.strftime('%B'))
-          find(:xpath, "//select[@id='#{base_dom_id}3i']").select(date.day.to_s)
+          find(:xpath, "//select[@id='#{base_dom_id}_1i']").select(date.year.to_s)
+          find(:xpath, "//select[@id='#{base_dom_id}_2i']").select(date.strftime('%B'))
+          find(:xpath, "//select[@id='#{base_dom_id}_3i']").select(date.day.to_s)
         end
       
         def select_time(field, options = {})
           time        = Time.zone.parse(options[:with])
           base_dom_id = get_base_dom_id_from_label_tag(field)
 
-          find(:xpath, "//select[@id='#{base_dom_id}4i']").select(time.hour.to_s.rjust(2, '0'))
-          find(:xpath, "//select[@id='#{base_dom_id}5i']").select(time.min.to_s.rjust(2,  '0'))
+          find(:xpath, "//select[@id='#{base_dom_id}_4i']").select(time.hour.to_s.rjust(2, '0'))
+          find(:xpath, "//select[@id='#{base_dom_id}_5i']").select(time.min.to_s.rjust(2,  '0'))
         end
       
         def select_datetime(field, options = {})
