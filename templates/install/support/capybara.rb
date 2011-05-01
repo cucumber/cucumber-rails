f21c346c0ebf400b176da8c9ce76210215a92723
Jay Levitt <jay-github@shopwatch.org>
2011-05-01 08:14:18 -0700

    Only set Capybara.default_selector if Capybara is loaded

--- templates/install/support/capybara.rb
+++ templates/install/support/capybara.rb
@@ -2,4 +2,4 @@
 # order to ease the transition to Capybara we set the default here. If you'd
 # prefer to use XPath just remove this line and adjust any selectors in your
 # steps to use the XPath syntax.
-Capybara.default_selector = :css
+Capybara.default_selector = :css if Object.const_defined?("Capybara")
