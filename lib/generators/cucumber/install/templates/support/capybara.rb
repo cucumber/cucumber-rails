662c7af7f992d87c493516559c8b95cbdb039f00
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2011-05-17 08:03:07 +0100

    Fix for Capybara name error from env.rb. Closes #125

--- lib/generators/cucumber/install/templates/support/capybara.rb
+++ lib/generators/cucumber/install/templates/support/capybara.rb
@@ -1,3 +1,4 @@
+require 'capybara'
 # Capybara defaults to XPath selectors rather than Webrat's default of CSS3. In
 # order to ease the transition to Capybara we set the default here. If you'd
 # prefer to use XPath just remove this line and adjust any selectors in your
