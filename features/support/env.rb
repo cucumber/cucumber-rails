c755666cb9263e29817474c334da2ff84dc985c2
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2011-05-20 00:17:18 +0100

    I think I need to reboot my machine

--- features/support/env.rb
+++ features/support/env.rb
@@ -5,6 +5,6 @@ require 'rspec/expectations'
 require 'aruba/cucumber'
 
 Before do
-  @aruba_timeout_seconds = 60 # A long time needed some times to install gems (bundle install)
+  @aruba_timeout_seconds = 120 # A long time needed some times
   unset_bundler_env_vars
 end
