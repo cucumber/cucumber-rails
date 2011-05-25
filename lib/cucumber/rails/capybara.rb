2d512bbb99c4ab29b1dad5911038273ed09ab434
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2011-05-25 18:51:43 +0100

    Make sure Capybara is always loaded. Fixes #143.

--- lib/cucumber/rails/capybara.rb
+++ lib/cucumber/rails/capybara.rb
@@ -1,7 +1,6 @@
-if defined?(Capybara)
-  require 'capybara/rails'
-  require 'capybara/cucumber'
-  require 'capybara/session'
-  require 'cucumber/rails/capybara/javascript_emulation'
-  require 'cucumber/rails/capybara/select_dates_and_times'
-end
+require 'capybara'
+require 'capybara/rails'
+require 'capybara/cucumber'
+require 'capybara/session'
+require 'cucumber/rails/capybara/javascript_emulation'
+require 'cucumber/rails/capybara/select_dates_and_times'
--- lib/generators/cucumber/install/templates/support/capybara.rb
+++ lib/generators/cucumber/install/templates/support/capybara.rb
@@ -1,4 +1,3 @@
-require 'capybara'
 # Capybara defaults to XPath selectors rather than Webrat's default of CSS3. In
 # order to ease the transition to Capybara we set the default here. If you'd
 # prefer to use XPath just remove this line and adjust any selectors in your
