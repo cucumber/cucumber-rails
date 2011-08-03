9e64e7c5bba5517b682a3f3513802d2f31eeff90
Doug Hiebert <dhiebert@winnipeg.ca>
2011-08-03 10:48:56 -0500

    Fix issue #152.

--- lib/cucumber/rails/hooks/active_record.rb
+++ lib/cucumber/rails/hooks/active_record.rb
@@ -1,21 +1,27 @@
 if defined?(ActiveRecord::Base)
   class ActiveRecord::Base
-    mattr_accessor :shared_connection
-    @@shared_connection = nil
+    class_inheritable_accessor :shared_connection
 
     def self.connection
-      @@shared_connection || retrieve_connection
+      self.shared_connection || retrieve_connection
     end
   end
-
+  
   Before('@javascript') do
-    # Forces all threads to share the same connection. This works on
-    # Capybara because it starts the web server in a thread.
-    ActiveRecord::Base.shared_connection = ActiveRecord::Base.connection
-  end
+     # Forces all threads to share a connection on a per-model basis,
+     # as connections may vary per model as per establish_connection. This works
+     # on Capybara because it starts the web server in a thread.
+     ActiveRecord::Base.shared_connection = ActiveRecord::Base.connection
+     ActiveRecord::Base.descendants.each do |model|
+       model.shared_connection = model.connection
+     end
+   end
 
   Before('~@javascript') do
     # Do not use a shared connection unless we're in a @javascript scenario
     ActiveRecord::Base.shared_connection = nil
+    ActiveRecord::Base.descendants.each do |model|
+       model.shared_connection = nil
+     end
   end
 end
