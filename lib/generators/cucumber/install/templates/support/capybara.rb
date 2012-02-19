e25e44ef428f3d2ff6d3021f8dc880fd91b6d837
Cezary Baginski <cezary.baginski@gmail.com>
2012-02-20 00:41:29 +0100

    update misleading capybara generator

--- lib/generators/cucumber/install/templates/support/capybara.rb
+++ lib/generators/cucumber/install/templates/support/capybara.rb
@@ -1,5 +1,4 @@
-# Capybara defaults to XPath selectors rather than Webrat's default of CSS3. In
-# order to ease the transition to Capybara we set the default here. If you'd
-# prefer to use XPath just remove this line and adjust any selectors in your
-# steps to use the XPath syntax.
-Capybara.default_selector = :css
+# Capybara and Webrat default to CSS3 selectors rather than XPath.
+# If you'd prefer to use XPath, just uncomment this line and adjust any
+# selectors in your steps to use the XPath syntax.
+# Capybara.default_selector = :xpath
