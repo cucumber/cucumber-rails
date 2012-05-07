713e34f7f77c1a2c7d4b83b71c9f2b722d82bead
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2012-05-07 16:15:35 -0500

    Words

--- lib/generators/cucumber/install/templates/support/capybara.rb
+++ lib/generators/cucumber/install/templates/support/capybara.rb
@@ -1,4 +1,4 @@
-# Capybara and Webrat default to CSS3 selectors rather than XPath.
+# Capybara defaults to CSS3 selectors rather than XPath.
 # If you'd prefer to use XPath, just uncomment this line and adjust any
-# selectors in your steps to use the XPath syntax.
+# selectors in your step definitions to use the XPath syntax.
 # Capybara.default_selector = :xpath
