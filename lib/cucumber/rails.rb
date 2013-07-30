c42eb64e7b43ceed72018e0e0ca0d3a293a0a195
Jon Rowe <hello@jonrowe.co.uk>
2013-07-30 22:16:58 +1000

    Remove `test_help` entirely
    Turns out for the Rails app's i've tried you don't need these lines at
    all. Given we're relying on spinning up the app via rack-test.

--- lib/cucumber/rails.rb
+++ lib/cucumber/rails.rb
@@ -7,13 +7,6 @@ if env_caller
   require File.expand_path(ENV["RAILS_ROOT"] + '/config/environment')
   require 'cucumber/rails/action_controller'
 
-  if defined?(ActiveRecord::Base)
-    require 'cucumber/rails/test_help'
-  else
-    require 'action_dispatch/testing/test_process'
-    require 'action_dispatch/testing/integration'
-  end
-
   if !Rails.application.config.cache_classes
     warn "WARNING: You have set Rails' config.cache_classes to false (most likely in config/environments/cucumber.rb).  This setting is known to cause problems with database transactions. Set config.cache_classes to true if you want to use transactions.  For more information see https://rspec.lighthouseapp.com/projects/16211/tickets/165."
   end
