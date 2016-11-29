f0dabc953ae6ff54e237517d732d6ff3f76d39db
Matijs van Zuijlen <matijs@matijs.net>
2016-11-29 20:50:30 +0100

    Ensure Rails stuff is present before ammeter needs it

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,4 +4,6 @@ require 'rails/version'
 require 'rspec/rails/fixture_support'
 # Avoid load order issues in activesupport
 require 'active_support/deprecation'
+# Ensure Rails stuff is present before ammeter needs it
+require 'rails/all'
 require 'ammeter/init'
