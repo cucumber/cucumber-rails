323953268bf2e6dd3d350cd4783520ef1ddb1798
Andrew <andrew@xtrasimplicity.com>
2018-02-12 22:28:33 +1100

    Restored DatabaseCleaner hook. (#358)

--- lib/cucumber/rails/hooks/database_cleaner.rb
+++ lib/cucumber/rails/hooks/database_cleaner.rb
@@ -1,11 +1,11 @@
 begin
   require 'database_cleaner'
 
-  Before('not @no-database-cleaner') do
+  Before('~@no-database-cleaner') do
     DatabaseCleaner.start if Cucumber::Rails::Database.autorun_database_cleaner
   end
 
-  After('not @no-database-cleaner') do
+  After('~@no-database-cleaner') do
     DatabaseCleaner.clean if Cucumber::Rails::Database.autorun_database_cleaner
   end
 
