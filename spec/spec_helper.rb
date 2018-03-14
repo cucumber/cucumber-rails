a0919d74ba50edd72a23fba8c249098f3e6bcf15
Matijs van Zuijlen <matijs@matijs.net>
2018-03-14 20:35:57 +0100

    Ensure Rails.application exists before loading ammeter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,12 @@
-require 'rubygems'
-
-require 'rails/version'
 require 'rspec/rails/fixture_support'
-# Avoid load order issues in activesupport
-require 'active_support/deprecation'
-# Ensure Rails stuff is present before ammeter needs it
 require 'rails/all'
+
+module CucumberRails
+  class Application < ::Rails::Application
+    self.config.secret_key_base = 'ASecretString' if config.respond_to? :secret_key_base
+  end
+end
+
+require 'rspec/support/spec'
+require 'rspec/rails'
 require 'ammeter/init'
