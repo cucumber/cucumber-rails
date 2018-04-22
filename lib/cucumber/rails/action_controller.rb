6ade6d0a49d43d3c5de6615f6eb4684b8d4116a8
Andrew <andrew@xtrasimplicity.com>
2018-04-22 19:32:50 +1000

    Removed check for Rails version >= 3.2.1
    As we don't support Rails 3 in Cucumber-rails >= 1.5

--- lib/cucumber/rails/action_controller.rb
+++ lib/cucumber/rails/action_controller.rb
@@ -7,9 +7,7 @@ class ActionDispatch::ShowExceptions
 
   def call(env)
     env['action_dispatch.show_exceptions'] = !!ActionController::Base.allow_rescue
-    if Rails.version >= "3.2.1"
-      env['action_dispatch.show_detailed_exceptions'] = !ActionController::Base.allow_rescue
-    end
+    env['action_dispatch.show_detailed_exceptions'] = !ActionController::Base.allow_rescue
     __cucumber_orig_call__(env)
   end
 end
