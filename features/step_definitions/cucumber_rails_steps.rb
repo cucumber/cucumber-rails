aab1bd3c942e30a3eb14a5f3e8ea92262203bac7
David Rodríguez <deivid.rodriguez@riseup.net>
2018-11-29 12:23:11 +0100

    Increase default timeout to fix CI error

--- features/step_definitions/cucumber_rails_steps.rb
+++ features/step_definitions/cucumber_rails_steps.rb
@@ -98,9 +98,12 @@ end
 Given /^I force selenium to run Firefox in headless mode$/ do
   selenium_config = %{
     Capybara.register_driver :selenium do |app|
+      http_client = Selenium::WebDriver::Remote::Http::Default.new
+      http_client.read_timeout = 180
+
       browser_options = ::Selenium::WebDriver::Firefox::Options.new()
       browser_options.args << '--headless'
-      Capybara::Selenium::Driver.new(app, browser: :firefox, options: browser_options)
+      Capybara::Selenium::Driver.new(app, browser: :firefox, options: browser_options, http_client: http_client)
     end
   }
 
