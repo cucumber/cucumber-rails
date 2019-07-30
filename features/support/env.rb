d20c4b38a69ddf213342b56d63169660f302d2b8
Luke Hill <lukehill_uk@hotmail.com>
2019-07-30 14:35:21 +0200

    Feng shui of step def code for testing
    Use a hooks.rb file for hooks
    Remove unrequired items in env.rb
    Delete unused helper in rails_helper.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,14 +1,6 @@
 # frozen_string_literal: true
 
 $LOAD_PATH.unshift(File.dirname(__FILE__) + '/../../lib')
-require 'rubygems'
-require 'bundler/setup'
+
 require 'rspec/expectations'
 require 'aruba/cucumber'
-
-After do |scenario|
-  if scenario.failed?
-    puts last_command_stopped.stdout
-    puts last_command_stopped.stderr
-  end
-end
