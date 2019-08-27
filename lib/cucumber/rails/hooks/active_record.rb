9f18bb437cd8039aaa4d9f19f67544211205ace3
Luke Hill <lukehill_uk@hotmail.com>
2019-08-27 10:08:12 +0200

    Since Rails 2.3 \(And possibly before\), ActiveRecord::Base has existed!

--- lib/cucumber/rails/hooks/active_record.rb
+++ lib/cucumber/rails/hooks/active_record.rb
@@ -1,25 +1,23 @@
 # frozen_string_literal: true
 
-if defined?(ActiveRecord::Base)
-  module ActiveRecord
-    class Base
-      class_attribute :shared_connection
+module ActiveRecord
+  class Base
+    class_attribute :shared_connection
 
-      def self.connection
-        shared_connection || retrieve_connection
-      end
+    def self.connection
+      shared_connection || retrieve_connection
     end
   end
+end
 
-  Before('@javascript') do
-    Cucumber::Rails::Database.before_js if Cucumber::Rails::Database.autorun_database_cleaner
-  end
+Before('@javascript') do
+  Cucumber::Rails::Database.before_js if Cucumber::Rails::Database.autorun_database_cleaner
+end
 
-  Before('not @javascript') do
-    Cucumber::Rails::Database.before_non_js if Cucumber::Rails::Database.autorun_database_cleaner
-  end
+Before('not @javascript') do
+  Cucumber::Rails::Database.before_non_js if Cucumber::Rails::Database.autorun_database_cleaner
+end
 
-  After do
-    Cucumber::Rails::Database.after if Cucumber::Rails::Database.autorun_database_cleaner
-  end
+After do
+  Cucumber::Rails::Database.after if Cucumber::Rails::Database.autorun_database_cleaner
 end
