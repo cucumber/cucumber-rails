409152fa1d744fffac270ff76e2090b941b30106
Matias Grunberg <matias@yellowspot.dev>
2022-03-15 23:47:57 -0300

    add support for alternative select boxes date field helpers
    I'm adding another spec forcing select boxes view helper. The helper has
    been around since Rails 5. It's not a problem to run the test in older
    versions

--- lib/cucumber/rails/capybara/select_dates_and_times.rb
+++ lib/cucumber/rails/capybara/select_dates_and_times.rb
@@ -8,12 +8,13 @@ module Cucumber
         # Select a Rails date. Options hash must include from: +label+
         def select_date(date, options)
           date = Date.parse(date)
-          if ::Rails::VERSION::MAJOR >= 7
-            # Rails 7 generates date fields using input type="date". Capybara support's them
+          base_dom_id = get_base_dom_from_options(options)
+
+          # Rails 7 use HTML5 input type="date" by default. If input is not present fallback to plain select boxes alternative.
+          # It's safe to use has_css? without waiting/retry. We already know field's label is visible
+          if html5_input_field_present?(base_dom_id)
             fill_in options[:from], with: date
           else
-            base_dom_id = get_base_dom_id_from_label_tag(options[:from])
-
             find(:xpath, ".//select[@id='#{base_dom_id}_1i']").select(date.year.to_s)
             find(:xpath, ".//select[@id='#{base_dom_id}_2i']").select(I18n.l(date, format: '%B'))
             find(:xpath, ".//select[@id='#{base_dom_id}_3i']").select(date.day.to_s)
@@ -23,12 +24,13 @@ module Cucumber
         # Select a Rails time. Options hash must include from: +label+
         def select_time(time, options)
           time = Time.zone.parse(time)
-          if ::Rails::VERSION::MAJOR >= 7
-            # Rails 7 generates date fields using input type="time". Capybara support's them
+          base_dom_id = get_base_dom_from_options(options)
+
+          # Rails 7 use HTML5 input type="time" by default. If input is not present fallback to plain select boxes alternative.
+          # It's safe to use has_css? without waiting/retry. We already know field's label is visible
+          if html5_input_field_present?(base_dom_id)
             fill_in options[:from], with: time
           else
-            base_dom_id = get_base_dom_id_from_label_tag(options[:from])
-
             find(:xpath, ".//select[@id='#{base_dom_id}_4i']").select(time.hour.to_s.rjust(2, '0'))
             find(:xpath, ".//select[@id='#{base_dom_id}_5i']").select(time.min.to_s.rjust(2,  '0'))
           end
@@ -36,16 +38,28 @@ module Cucumber
 
         # Select a Rails datetime. Options hash must include from: +label+
         def select_datetime(datetime, options)
-          if ::Rails::VERSION::MAJOR >= 7
-            # Rails 7 generates datetime fields using input type="datetime-local". Capybara support's them
+          base_dom_id = get_base_dom_id_from_label_tag(options[:from])
+
+          # Rails 7 use HTML5 input type="datetime-local" by default. If input is not present fallback to plain select boxes alternative.
+          # It's safe to use has_css? without waiting/retry. We already know field's label is visible
+          if html5_input_field_present?(base_dom_id)
             fill_in options[:from], with: DateTime.parse(datetime)
           else
-            select_date(datetime, options)
-            select_time(datetime, options)
+            extended_options = options.merge(base_dom_id: base_dom_id)
+            select_date(datetime, extended_options)
+            select_time(datetime, extended_options)
           end
         end
 
         private
+
+        def html5_input_field_present?(base_dom_id)
+          ::Rails::VERSION::MAJOR >= 7 && page.has_css?("##{base_dom_id}", wait: 0)
+        end
+
+        def get_base_dom_from_options(options)
+          options[:base_dom_id] || get_base_dom_id_from_label_tag(options[:from])
+        end
 
         # @example "event_starts_at_"
         def get_base_dom_id_from_label_tag(field)
