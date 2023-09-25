a8ae4bdb26acc0e70d2bde08cde3e23836a49af6
Luke Hill <lukehill_uk@hotmail.com>
2023-09-25 16:21:05 +0100

    Re-order file contents

--- features/support/cucumber_rails_helper.rb
+++ features/support/cucumber_rails_helper.rb
@@ -32,6 +32,48 @@ module CucumberRailsHelper
 
   private
 
+  def run_rails_new_command(options)
+    options[:name] ||= 'test_app'
+    flags = %w[--skip-action-cable --skip-action-mailer --skip-active-job --skip-bootsnap --skip-bundle --skip-javascript
+               --skip-jbuilder --skip-listen --skip-spring --skip-sprockets --skip-test-unit --skip-turbolinks --skip-active-storage]
+    flags += %w[--skip-action-mailbox --skip-action-text] if rails_equal_or_higher_than?('6.0')
+    run_command "bundle exec rails new #{options[:name]} #{flags.join(' ')} #{options[:args]}"
+  end
+
+  def configure_rails_gems
+    %w[bootsnap byebug jbuilder listen rails sass-rails turbolinks webpacker].each { |gem| remove_gem(gem) }
+    %w[railties activerecord actionpack].each { |rails_gem| add_gem(rails_gem, Rails.version) }
+  end
+
+  def configure_rails_requires
+    content = File.read(expand_path('config/application.rb'))
+    %w[active_job/railtie active_storage/engine action_mailer/railtie action_mailbox/engine
+       action_text/engine action_cable/engine rails/test_unit/railtie sprockets/railtie].each do |require|
+      content = content.gsub(/^.*require ["']#{require}["']\s*$/, '')
+    end
+    overwrite_file('config/application.rb', content)
+  end
+
+  def configure_rails_layout
+    file = 'app/views/layouts/application.html.erb'
+    content = File.read(expand_path(file)).gsub(/^\s*<%= stylesheet_link_tag .*%>\s*$/, '')
+    overwrite_file(file, content)
+  end
+
+  def clear_bundle_env_vars
+    unset_bundler_env_vars
+    delete_environment_variable 'BUNDLE_GEMFILE'
+  end
+
+  def rails_equal_or_higher_than?(version)
+    Rails.gem_version >= Gem::Version.new(version)
+  end
+
+  def remove_gem(name)
+    content = File.read(expand_path('Gemfile')).gsub(/^\s*gem ["']#{name}["'].*$/, '')
+    overwrite_file('Gemfile', content)
+  end
+
   def add_gem(name, *args)
     line = convert_gem_opts_to_string(name, *args)
     gem_regexp = /gem ["']#{name}["'].*$/
@@ -45,52 +87,12 @@ module CucumberRailsHelper
     end
   end
 
-  def remove_gem(name)
-    content = File.read(expand_path('Gemfile')).gsub(/^\s*gem ["']#{name}["'].*$/, '')
-    overwrite_file('Gemfile', content)
-  end
-
-  def bundle_install
-    run_command_and_stop 'bundle config set --local without "development"'
-    run_command_and_stop "bundle config set --local path '#{ENV.fetch('GITHUB_WORKSPACE')}/vendor/bundle'" if ENV.key?('GITHUB_WORKSPACE')
-    run_command_and_stop 'bundle install --jobs 4'
-  end
-
-  def configure_rails_gems
-    %w[bootsnap byebug jbuilder listen rails sass-rails turbolinks webpacker].each { |gem| remove_gem(gem) }
-    %w[railties activerecord actionpack].each { |rails_gem| add_gem(rails_gem, Rails.version) }
-  end
-
-  def configure_rails_requires
-    content = File.read(expand_path('config/application.rb'))
-    %w[ active_job/railtie active_storage/engine action_mailer/railtie action_mailbox/engine
-        action_text/engine action_cable/engine rails/test_unit/railtie sprockets/railtie ].each do |require|
-      content = content.gsub(/^.*require ["']#{require}["']\s*$/, '')
-    end
-    overwrite_file('config/application.rb', content)
-  end
-
-  def configure_rails_layout
-    file = 'app/views/layouts/application.html.erb'
-    content = File.read(expand_path(file)).gsub(/^\s*<%= stylesheet_link_tag .*%>\s*$/, '')
-    overwrite_file(file, content)
-  end
-
-  def run_rails_new_command(options)
-    options[:name] ||= 'test_app'
-    flags = %w[--skip-action-cable --skip-action-mailer --skip-active-job --skip-bootsnap --skip-bundle --skip-javascript
-               --skip-jbuilder --skip-listen --skip-spring --skip-sprockets --skip-test-unit --skip-turbolinks --skip-active-storage]
-    flags += %w[--skip-action-mailbox --skip-action-text] if rails_equal_or_higher_than?('6.0')
-    run_command "bundle exec rails new #{options[:name]} #{flags.join(' ')} #{options[:args]}"
-  end
-
-  def clear_bundle_env_vars
-    unset_bundler_env_vars
-    delete_environment_variable 'BUNDLE_GEMFILE'
-  end
-
-  def rails_equal_or_higher_than?(version)
-    Rails.gem_version >= Gem::Version.new(version)
+  def convert_gem_opts_to_string(name, *args)
+    options = args.last.is_a?(Hash) ? args.pop : {}
+    parts = ["'#{name}'"]
+    parts << args.map(&:inspect) if args.any?
+    parts << options.inspect[1..-2] if options.any?
+    "gem #{parts.flatten.join(', ')}\n"
   end
 
   def add_cucumber_rails(options)
@@ -109,12 +111,10 @@ module CucumberRailsHelper
     end
   end
 
-  def convert_gem_opts_to_string(name, *args)
-    options = args.last.is_a?(Hash) ? args.pop : {}
-    parts = ["'#{name}'"]
-    parts << args.map(&:inspect) if args.any?
-    parts << options.inspect[1..-2] if options.any?
-    "gem #{parts.flatten.join(', ')}\n"
+  def bundle_install
+    run_command_and_stop 'bundle config set --local without "development"'
+    run_command_and_stop "bundle config set --local path '#{ENV.fetch('GITHUB_WORKSPACE')}/vendor/bundle'" if ENV.key?('GITHUB_WORKSPACE')
+    run_command_and_stop 'bundle install --jobs 4'
   end
 end
 
