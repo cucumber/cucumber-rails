535b2ab8214a8e751315be57d5372a9529a8208c
Luke Hill <lukehill_uk@hotmail.com>
2023-10-11 23:29:06 +0100

    Fix issue where database wasn't loading the database folder

--- spec/cucumber/rails/database_spec.rb
+++ spec/cucumber/rails/database_spec.rb
@@ -1,11 +1,6 @@
 # frozen_string_literal: true
 
 require 'database_cleaner'
-require 'cucumber/rails/database/strategy'
-require 'cucumber/rails/database/deletion_strategy'
-require 'cucumber/rails/database/null_strategy'
-require 'cucumber/rails/database/shared_connection_strategy'
-require 'cucumber/rails/database/truncation_strategy'
 require 'cucumber/rails/database'
 
 describe Cucumber::Rails::Database do
@@ -29,15 +24,13 @@ describe Cucumber::Rails::Database do
     it 'raises an error on `before_js` if no DatabaseCleaner cleaners exist' do
       allow(DatabaseCleaner).to receive(:cleaners).and_return({})
 
-      expect { described_class.before_js }
-        .to raise_error(/No DatabaseCleaner strategies found/)
+      expect { described_class.before_js }.to raise_error(/No DatabaseCleaner strategies found/)
     end
   end
 
   context 'when using an invalid pre-determined strategy' do
     it 'raises an error if you use a non-understood strategy' do
-      expect { described_class.javascript_strategy = :invalid }
-        .to raise_error(Cucumber::Rails::Database::InvalidStrategy)
+      expect { described_class.javascript_strategy = :invalid }.to raise_error(Cucumber::Rails::Database::InvalidStrategy)
     end
   end
 
@@ -73,8 +66,7 @@ describe Cucumber::Rails::Database do
     let(:invalid_strategy) { Class.new }
 
     it 'raises an error if the strategy does not have a valid interface' do
-      expect { described_class.javascript_strategy = invalid_strategy }
-        .to raise_error(ArgumentError)
+      expect { described_class.javascript_strategy = invalid_strategy }.to raise_error(ArgumentError)
     end
   end
 end
